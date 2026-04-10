98567ac0ef6f52f69122fbea4e5b830c0ca65079
Ning <xiaoningning@yahoo.com>
2026-04-09 21:23:17 -0700

    1091

--- src/1091-shortest-path-binary-matrix.py
+++ src/1091-shortest-path-binary-matrix.py
@@ -4,24 +4,27 @@ from collections import deque
 
 
 class Solution:
-    def shortestPathBinaryMatrix(self, grid: list[list[int]]) -> int:
+    def shortestPathBinaryMatrix(self, grid: list[list[int]]) -> int:  # noqa: N802
         n = len(grid)
         if grid[0][0] != 0 or grid[-1][-1] != 0:
             return -1
 
         dist = 0
         q = deque([(0, 0)])
-        grid[0][0] = 1
+        grid[0][0] = 1 # mark as visited
 
         while q:
             for _ in range(len(q)):
-                row, col = q.pop()
+                # print(q)
+                row, col = q.popleft()
+
                 if row == n - 1 and col == n - 1:
                     return dist
 
                 for dr in range(-1, 2):
                     for dc in range(-1, 2):
-                        if dr == 0 and dc == 0:
+                        # no diagonal move
+                        if dr == 1 and dc == 1:
                             continue
 
                         nr, nc = row + dr, col + dc
@@ -29,8 +32,34 @@ class Solution:
                             grid[nr][nc] = 1
                             q.append((nr, nc))
             dist += 1
+
         return -1
 
+    def shortestPathBinaryMatrix2(self, grid: list[list[int]]) -> int:  # noqa: N802
+        if grid[0][0] == 1 or grid[-1][-1]:
+            return -1
+        
+        n = len(grid)
+        q = deque([(0,0,0)])
+        grid[0][0] = 1
+
+        while q:
+            # print(q)
+            x, y, d = q.popleft()
+            if x == n - 1 and y == n - 1:
+                return d
+            
+            for i in range(x - 1, x + 2):
+                for j in range(y - 1, y + 2):
+                    if i == x + 1 and j == y + 1:
+                        continue
+                        
+                    if 0 <= i < n and 0 <= j < n and grid[i][j] == 0:
+                        grid[i][j] = 1
+                        q.append((i, j, d + 1))
+        
+        return -1
+    
 
 if __name__ == '__main__':
     m1 = [
@@ -39,5 +68,5 @@ if __name__ == '__main__':
         [1, 0, 0],
     ]
 
-    r = Solution().shortestPathBinaryMatrix(m1)
+    r = Solution().shortestPathBinaryMatrix2(m1)
     print(f"shortest path: {r}")
